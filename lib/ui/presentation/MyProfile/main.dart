e16be68da89692653bdede19ca8c71aff8417233
jukaxd <127540718+jukaxd@users.noreply.github.com>
2023-11-27 21:58:05 -0600

    implementacion de modo oscuro

--- lib/ui/presentation/MyProfile/main.dart
+++ lib/ui/presentation/MyProfile/main.dart
@@ -12,12 +12,11 @@ class MyProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80.0),
-        child: AppBar(
+    return BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
+      double width = MediaQuery.of(context).size.width;
+      double height = MediaQuery.of(context).size.height;
+      return Scaffold(
+        appBar: AppBar(
           centerTitle: true,
           title: const Text(
             'Mi Cuenta',
@@ -35,127 +34,158 @@ class MyProfilePage extends StatelessWidget {
             },
             color: Colors.white,
           ),
-          backgroundColor: const Color.fromARGB(0, 71, 171, 1),
+          backgroundColor: state.isLightTheme
+              ? const Color.fromRGBO(0, 71, 171, 1)
+              : Colors.black,
         ),
-      ),
-      backgroundColor: const Color.fromRGBO(0, 71, 171, 1),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Container(
-            height: height * 0.8,
+        backgroundColor: state.isLightTheme
+            ? const Color.fromRGBO(0, 71, 171, 1)
+            : Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: height * 0.8,
 
-            //margin: EdgeInsets.symmetric(horizontal: 0),
-            child: LayoutBuilder(builder: (context, constraints) {
-              double innerHeight = constraints.maxHeight;
-              double innerWidth = constraints.maxWidth;
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  Positioned(
-                    //RECTANGULO GRIS
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      height: innerHeight * 0.9,
-                      width: innerWidth,
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(40),
-                            topRight: Radius.circular(40),
+              //margin: EdgeInsets.symmetric(horizontal: 0),
+              child: LayoutBuilder(builder: (context, constraints) {
+                double innerHeight = constraints.maxHeight;
+                double innerWidth = constraints.maxWidth;
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Positioned(
+                      //RECTANGULO GRIS
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: innerHeight * 0.9,
+                        width: innerWidth,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(40),
+                              topRight: Radius.circular(40),
+                            ),
+                            color: Color(0xffd9d9d9)),
+                        child: Container(
+                          //datos personales
+                          //height: 10,
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 100),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.circular(40), // Bordes circulares
                           ),
-                          color: Color(0xffd9d9d9)),
-                      child: Container(
-                        //datos personales
-                        //height: 10,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 100),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius:
-                              BorderRadius.circular(40), // Bordes circulares
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-                          children: [
-                            Container(
-                              child: const Text(
-                                'Enzo Fernandez',
-                                style: TextStyle(
-                                    fontSize: 40,
-                                    fontFamily: 'Nunito',
-                                    color: Colors.black),
-                                textAlign: TextAlign.left,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                            children: [
+                              Container(
+                                child: const Text(
+                                  'Enzo Fernandez',
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      fontFamily: 'Nunito',
+                                      color: Colors.black),
+                                  textAlign: TextAlign.left,
+                                ),
                               ),
-                            ),
-                            Container(
-                              // margin: const EdgeInsets.symmetric(horizontal: 2),
-                              child: const Text(
-                                'Correo electrónico: user@gmail.com',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontFamily: 'Nunito',
-                                    color: Colors.black),
+                              Container(
+                                // margin: const EdgeInsets.symmetric(horizontal: 2),
+                                child: const Text(
+                                  'Correo electrónico: user@gmail.com',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'Nunito',
+                                      color: Colors.black),
+                                ),
                               ),
-                            ),
-                            Container(
-                              child: const Text(
-                                'De: CDMX, México',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontFamily: 'Nunito',
-                                    color: Colors.black),
-                                textAlign: TextAlign.left,
+                              Container(
+                                child: const Text(
+                                  'De: CDMX, México',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'Nunito',
+                                      color: Colors.black),
+                                  textAlign: TextAlign.left,
+                                ),
                               ),
-                            ),
-                            Container(
-                              child: const Text(
-                                'Teléfono: 55-1111-1111',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontFamily: 'Nunito',
-                                    color: Colors.black),
+                              Container(
+                                child: const Text(
+                                  'Teléfono: 55-1111-1111',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'Nunito',
+                                      color: Colors.black),
+                                ),
                               ),
-                            ),
-                            Container(
-                              child: const Text(
-                                'Contraseña: *****',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontFamily: 'Nunito',
-                                    color: Colors.black),
+                              Container(
+                                child: const Text(
+                                  'Contraseña: *****',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'Nunito',
+                                      color: Colors.black),
+                                ),
                               ),
-                            ),
-                          ],
-                          //
+                            ],
+                            //
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    ///IMAGEN DE PERFIL
-                    top: 0,
-                    left: 0,
-                    right: 90,
-                    child: Center(
-                      child: Container(
-                        width: 150,
-                        height: 150,
-                        child: const CircleAvatar(
-                          backgroundImage:
-                              AssetImage('assets/MyProfile/yoyo.JPG'),
+                    Positioned(
+                      ///IMAGEN DE PERFIL
+                      top: 0,
+                      left: 0,
+                      right: 90,
+                      child: Center(
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: const CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assets/MyProfile/yoyo.JPG'),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            }),
-          ),
-        ],
-      ),
-    );
+                    Container(
+                      margin: EdgeInsets.only(
+                        bottom: 470,
+                        left: 280,
+                        right: 10,
+                      ),
+                      child: Row(
+                        children: [
+                          const Text(
+                            'Editar',
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.black,
+                                fontSize: 27,
+                                fontFamily: 'Nunito',
+                                color: Colors.black),
+                            textAlign: TextAlign.left,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            color: Colors.black,
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }

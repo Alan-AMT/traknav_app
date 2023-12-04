e011b72b788d9729a2862fe2868f3a9544356757
AngelVC <yaguragod@gmail.com>
2023-12-04 16:39:01 -0600

    Idioma implementado para FAQs

--- lib/ui/presentation/FAQs/problemasTecnicos/problemasTecnicos.dart
+++ lib/ui/presentation/FAQs/problemasTecnicos/problemasTecnicos.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:traknav_app/ui/presentation/FAQs/actionArea.dart';
 import 'package:traknav_app/ui/presentation/FAQs/problemasTecnicos/technicalQuestions.dart';
 import 'package:traknav_app/ui/presentation/FAQs/topScreenBar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 @RoutePage()
 class ProblemasTecnicosPage extends StatelessWidget {
@@ -12,13 +13,16 @@ class ProblemasTecnicosPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: const Text("PREGUNTAS FRECUENTES")),
+        appBar: AppBar(
+            title: Text(AppLocalizations.of(context)!.navBarTitle,
+                style: const TextStyle(fontSize: 18))),
         body: ListView(
           children: <Widget>[
             //const TopScreenBar(),
             Image.asset('assets/FAQs/FAQs.png'),
-            const ActionArea(
-                text: "PERFIL DE USUARIO", content: TechnicalQuestions())
+            ActionArea(
+                text: AppLocalizations.of(context)!.tecProblems,
+                content: TechnicalQuestions())
           ],
         ),
       ),

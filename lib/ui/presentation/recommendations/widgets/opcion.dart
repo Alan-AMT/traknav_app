6947fb50acce59526b6522c95137195983cd7f59
Hiram-hr <118991748+Hiram-hr@users.noreply.github.com>
2023-12-14 18:41:30 -0600

    Recomendaciones con cambio de idioma y modo oscuro al 100

--- lib/ui/presentation/recommendations/widgets/opcion.dart
+++ lib/ui/presentation/recommendations/widgets/opcion.dart
@@ -97,122 +97,135 @@ class _BotonOpcionState extends State<BotonOpcion> {
   }
 
   Widget build(BuildContext context) {
-    return Column( children:[
-        Center(
-        child:
-        Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-        height: MediaQuery.of(context).size.height * 0.79,
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 58, 172, 255),
-            
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child:
-         Column(children: [
-          Expanded(
-              child: GridView.builder(
-                  itemCount: opciones.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: ((context, index) {
-                    return GestureDetector(
-                        onTap: () {
-                        // Cambiar el estado al opuesto cada vez que se toca el Container
-                          setState(() {
-                            toggleCategoria(index);
-                            
-                          });
-                        },
-                        child: Container(
-                            margin: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                color: opciones[index].estaSeleccionado!?
-                                Colors.blue[700]: 
-                                const Color.fromARGB(180, 230, 230, 230),
-                                borderRadius: BorderRadius.circular(10)),
-                            child:Column( 
-                                  mainAxisAlignment: MainAxisAlignment.center, 
-                                  children: [
-                                    Image.asset('assets/${opciones[index].foto}', 
-                                      width: 100), 
-                                    Text( 
-                                      opciones[index].nombre, 
-                                      style: 
-                                        TextStyle(
-                                          color: opciones[index].estaSeleccionado!?
-                                          Colors.white: 
-                                          Colors.black,
-                                        ), 
-                                      ),
-                                    
-                                  ], 
-                          ),
-                                ),
-                    );
-                  })
-                  )
-                  ),
-          Row(
-            children: [
-              Expanded(flex : 5, child:
+    final nombres = [
+      //**Volver a coloca AppLocalizations...
+      AppLocalizations.of(context)!.recPopular,
+      AppLocalizations.of(context)!.recMuseum,
+      AppLocalizations.of(context)!.recRestaurants,
+      AppLocalizations.of(context)!.recLocalCommerce,
+      AppLocalizations.of(context)!.recHistorical,
+      AppLocalizations.of(context)!.recLodgment,
+      AppLocalizations.of(context)!.recParks,
+      AppLocalizations.of(context)!.recGuided,
+    ];
+    return BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
+      return Column( children:[
+          Center(
+          child:
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.height * 0.79,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 58, 172, 255),
               
-              
-              Text(countSelected().toString()+'/3 seleccionados',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize:
-                            Theme.of(context).textTheme.titleLarge!.fontSize)),
-              
-              //Expanded(child: 
-              ),
-              Expanded(flex : 4, child:
-              ElevatedButton(
-                /*onPressed: () async {
-                  agregarPreferencias();
-                  mostrarPreferencias();
-                    await onFormSent(context);
-                  },
-                  */
-                onPressed: () async {
-                  
-                  agregarPreferencias();
-                  mostrarPreferencias();
-
-                  User? user = auth.currentUser;
-                  if(user != null){
-                    String uid = user.uid;
-                    await FirebaseFirestore.instance.collection('users').doc(uid).update(
-                      {'recommendations': prefs.preferenciasSeleccionadas}
-                    );
-                  }
-
-                  AutoRouter.of(context).navigate(const HomeRoute());
-                },
-                style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blue),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ))),
-                child: Text('Siguiente',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize:
-                            Theme.of(context).textTheme.titleLarge!.fontSize)),
-              )
-              ),
-
-             //)
-            ],
+            borderRadius: BorderRadius.circular(20),
           ),
-          
-        ]
-        )
-        ),
-        ),
-        const ButtonOmitir(),
-        ]
-        );
+          child:
+           Column(children: [
+            Expanded(
+                child: GridView.builder(
+                    itemCount: opciones.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemBuilder: ((context, index) {
+                      return GestureDetector(
+                          onTap: () {
+                          // Cambiar el estado al opuesto cada vez que se toca el Container
+                            setState(() {
+                              toggleCategoria(index);
+                              
+                            });
+                          },
+                          child: Container(
+                              margin: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: opciones[index].estaSeleccionado!?
+                                  Colors.blue[700]: 
+                                  const Color.fromARGB(180, 230, 230, 230),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child:Column( 
+                                    mainAxisAlignment: MainAxisAlignment.center, 
+                                    children: [
+                                      Image.asset('assets/${opciones[index].foto}', 
+                                        width: 100), 
+                                      Text( 
+                                        nombres[index], 
+                                        style: 
+                                          TextStyle(
+                                            color: opciones[index].estaSeleccionado!?
+                                            Colors.white: 
+                                            Colors.black,
+                                          ), 
+                                        ),
+                                      
+                                    ], 
+                            ),
+                                  ),
+                      );
+                    })
+                    )
+                    ),
+            Row(
+              children: [
+                Expanded(flex : 5, child:
+                
+                
+                Text(countSelected().toString()+'/3 '+ AppLocalizations.of(context)!.recSelected,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize:
+                              Theme.of(context).textTheme.titleLarge!.fontSize)),
+                
+                //Expanded(child: 
+                ),
+                Expanded(flex : 4, child:
+                ElevatedButton(
+                  /*onPressed: () async {
+                    agregarPreferencias();
+                    mostrarPreferencias();
+                      await onFormSent(context);
+                    },
+                    */
+                  onPressed: () async {
+                    
+                    agregarPreferencias();
+                    mostrarPreferencias();
+    
+                    User? user = auth.currentUser;
+                    if(user != null){
+                      String uid = user.uid;
+                      await FirebaseFirestore.instance.collection('users').doc(uid).update(
+                        {'recommendations': prefs.preferenciasSeleccionadas}
+                      );
+                    }
+    
+                    AutoRouter.of(context).navigate(const HomeRoute());
+                  },
+                  style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ))),
+                  child: Text(AppLocalizations.of(context)!.recNextButton,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize:
+                              Theme.of(context).textTheme.titleLarge!.fontSize)),
+                )
+                ),
+    
+               //)
+              ],
+            ),
+            
+          ]
+          )
+          ),
+          ),
+          const ButtonOmitir(),
+          ]
+          );
+   });
   }
 }

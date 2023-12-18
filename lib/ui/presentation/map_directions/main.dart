6bec41f7c12ded37b277f7857f3ac586bc2efd27
José Costa <88689761+Jose-Costa-M@users.noreply.github.com>
2023-12-18 10:48:09 -0600

    Cambios de julio en cambiar opcion de ruta

--- lib/ui/presentation/map_directions/main.dart
+++ lib/ui/presentation/map_directions/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,6 +7,9 @@ import 'package:traknav_app/ui/presentation/map_search/cubit/map_search_cubit.da
 import 'package:location/location.dart';
 import 'package:google_maps_widget/google_maps_widget.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+
+enum TransportMode { walking, driving, transit }
 
 @RoutePage()
 class MapDirectionsPage extends StatefulWidget {
@@ -23,16 +27,89 @@ class MapDirectionsPage extends StatefulWidget {
 }
 
 class _MapDirectionsPage extends State<MapDirectionsPage> {
+  late TransportMode _selectedMode; // Definición de la variable
   String distanceLeft = "0";
   String timeLeft = "0";
   String arrivalTime = "0";
+  List<Polyline> myPolylines = [];
   final mapsWidgetController = GlobalKey<GoogleMapsWidgetState>();
+  ValueKey<int> mapKey = ValueKey(DateTime.now().millisecondsSinceEpoch);
+
+  Future<void> _getRoute(TransportMode mode) async {
+    String modeValue = _getModeValue(mode);
+    String baseUrl =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=${widget.sourceCoords.latitude},${widget.sourceCoords.longitude}&destination=${widget.destinyCoords.latitude},${widget.destinyCoords.longitude}&mode=$modeValue&key=AIzaSyDrlBOulG7pFmv031_ki6AIBdB31trUvzI";
+
+    try {
+      final response = await http.get(Uri.parse(baseUrl));
+      if (response.statusCode == 200) {
+        var jsonData = json.decode(response.body);
+        var routes = jsonData['routes'];
+
+        if (routes.isNotEmpty) {
+          var encodedPolyline = routes[0]['overview_polyline']['points'];
+          var polylinePoints = _decodePolyline(encodedPolyline);
+          var leg = routes[0]['legs'][0];
+          var distance = leg['distance']['text'];
+          var duration = leg['duration']['text'];
+          var durationValue = leg['duration']['value'];
+          var arrivalDateTime =
+              DateTime.now().add(Duration(seconds: durationValue));
+          var formattedArrivalTime = DateFormat.Hm().format(arrivalDateTime);
+
+          setState(() {
+            myPolylines = [
+              Polyline(
+                polylineId: PolylineId('new_route'),
+                color: Colors.blue,
+                points: polylinePoints,
+                width: 5,
+              )
+            ];
+            // Actualiza la información de distancia, tiempo y hora de llegada
+            distanceLeft = distance;
+            timeLeft = duration;
+            arrivalTime = formattedArrivalTime;
+          });
+        }
+      } else {
+        setState(() {
+          distanceLeft = "Error";
+          timeLeft = "Error";
+          arrivalTime = "Error";
+          myPolylines = [];
+        });
+        print("Error al obtener la ruta: ${response.statusCode}");
+      }
+    } catch (e) {
+      // Manejo de errores en la solicitud HTTP.
+      setState(() {
+        distanceLeft = "Error";
+        timeLeft = "Error";
+        arrivalTime = "Error";
+        myPolylines = [];
+      });
+      print("Error al conectar con la API: $e");
+    }
+  }
+
+  String _getModeValue(TransportMode mode) {
+    switch (mode) {
+      case TransportMode.walking:
+        return "walking";
+      case TransportMode.driving:
+        return "driving";
+      case TransportMode.transit:
+        return "transit";
+      default:
+        return "driving";
+    }
+  }
 
   Future<LocationData?> _getLocation() async {
     Location location = Location();
     return await location.getLocation();
   }
-
 
   void _selectTransportMode() {
     showDialog(
@@ -71,15 +148,74 @@ class _MapDirectionsPage extends State<MapDirectionsPage> {
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    _selectedMode =
+        TransportMode.driving; // Solo se establece aquí inicialmente
+    _getRoute(_selectedMode); // Solicita la ruta inicial
+  }
+
+  // Este método se llama cuando el usuario selecciona un nuevo modo de transporte
+  void onTransportModeSelected(TransportMode mode) {
+    if (_selectedMode != mode) {
+      setState(() {
+        _selectedMode = mode;
+      });
+      _getRoute(_selectedMode); // Solicita la nueva ruta
+    }
+  }
+
+  void _updateRoute(TransportMode mode) {
+    setState(() {
+      _selectedMode = mode;
+      distanceLeft = "0";
+      timeLeft = "0";
+      arrivalTime = "0";
+    });
+    _getRoute(mode);
+  }
+
+  List<LatLng> _decodePolyline(String encoded) {
+    List<LatLng> points = [];
+    int index = 0, len = encoded.length;
+    int lat = 0, lng = 0;
+
+    while (index < len) {
+      int b, shift = 0, result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      LatLng p = LatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
+      points.add(p);
+    }
+
+    return points;
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<MapSearchCubit, MapSearchState>(
-        builder: (context, state) {
-      return Scaffold(
+      builder: (context, state) {
+        return Scaffold(
           appBar: AppBar(
             title: Text(widget.destinationName),
-          )
+          ),
           body: Column(
             children: [
               Expanded(
@@ -110,12 +246,13 @@ class _MapDirectionsPage extends State<MapDirectionsPage> {
                   updatePolylinesOnDriverLocUpdate: true,
                   driverCoordinatesStream: Stream.periodic(
                     const Duration(milliseconds: 5000),
-                        (i) {
+                    (i) {
                       return _getLocation();
                     },
                   ).asyncMap((event) async {
                     final location = await event;
-                    return LatLng(location?.latitude ?? 0, location?.longitude ?? 0);
+                    return LatLng(
+                        location?.latitude ?? 0, location?.longitude ?? 0);
                   }),
                   totalDistanceCallback: (newDistance) => setState(() {
                     distanceLeft = newDistance ?? "0";
@@ -127,7 +264,8 @@ class _MapDirectionsPage extends State<MapDirectionsPage> {
                     }
                     String hoursToAdd = "0";
                     final DateTime now = DateTime.now();
-                    final List<String> reversed = newTime.toString().split(" ").reversed.toList();
+                    final List<String> reversed =
+                        newTime.toString().split(" ").reversed.toList();
                     final minutesToAdd = reversed[1];
                     if (reversed.length > 2) {
                       hoursToAdd = reversed[3];
@@ -144,23 +282,33 @@ class _MapDirectionsPage extends State<MapDirectionsPage> {
                   onPolylineUpdate: (Polyline polyline) {
                     print("Polyline updated");
                   },
-
-            ListTile(
-                subtitle: Text("$distanceLeft  \u2022  $arrivalTime",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 20)),
-                title: Text(timeLeft,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25))),
-          ]),
-              floatingActionButton: FloatingActionButton(
+                ),
+              ),
+              ListTile(
+                subtitle: Text(
+                  "$distanceLeft  \u2022  $arrivalTime",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                title: Text(
+                  timeLeft,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
             onPressed: _selectTransportMode,
             child: Icon(Icons.directions),
             tooltip: 'Seleccionar Medio de Transporte',
-          ),);
-    });
+          ),
+        );
+      },
+    );
   }
 }

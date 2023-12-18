40954e74661c73bbe3760b068b1372a5225e8636
Alan Munoz <alanx015@hotmail.com>
2023-12-18 11:09:51 -0600

    add map directios to home form

--- lib/ui/presentation/home/widgets/searchBar.dart
+++ lib/ui/presentation/home/widgets/searchBar.dart
@@ -1,7 +1,13 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:location/location.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:traknav_app/ui/presentation/map_search/widgets/search_form.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:traknav_app/ui/core/data/map_search.dart' as dataSource;
+import 'package:traknav_app/ui/router/android.gr.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SearchBarWidget extends StatefulWidget {
   const SearchBarWidget({Key? key}) : super(key: key);
@@ -12,6 +18,34 @@ class SearchBarWidget extends StatefulWidget {
 
 class _SearchBarWidgetState extends State<SearchBarWidget> {
   String? homeSearchBarText;
+
+  Future<bool> checkPermissions() async {
+    bool serviceEnabled;
+    Location location = Location();
+    PermissionStatus permissionGranted;
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return false;
+      }
+    }
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        false;
+      }
+    }
+    return true;
+  }
+
+  Future<LocationData?> _getLocation() async {
+    final bool hasPermission = await checkPermissions();
+    if (!hasPermission) return null;
+    Location location = Location();
+    return await location.getLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +69,25 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
       ),
       child: TextField(
         onTap: () async {
-          final String? placeId = await showMaterialModalBottomSheet(
+          final <Object?>[placeId, mainText, secText, ..._] =
+              await showMaterialModalBottomSheet(
             context: context,
             builder: (context) => const SearchForm(),
           );
-          print("******************");
-          print(placeId);
           if (placeId == null) return;
+          await EasyLoading.show();
+          final details =
+              await dataSource.getPlaceDetails(placeId: placeId.toString());
+          final targetPosition =
+              LatLng(details.location.latitude, details.location.longitude);
+          final LocationData? currentLocation = await _getLocation();
+          await EasyLoading.dismiss();
+          // ignore: use_build_context_synchronously
+          context.router.push(MapDirectionsRoute(
+              destinyCoords: targetPosition,
+              sourceCoords: LatLng(currentLocation?.latitude ?? 0,
+                  currentLocation?.longitude ?? 0),
+              destinationName: mainText.toString()));
         },
         cursorColor: Colors.white,
         decoration: InputDecoration(
